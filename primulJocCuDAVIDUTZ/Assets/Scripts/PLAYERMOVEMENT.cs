1cf9dad070d7fc3392e3305e70829583ef003d77
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-11 09:42:43 +0200

    NUSH

---
+++ primulJocCuDAVIDUTZ/Assets/Scripts/PLAYERMOVEMENT.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb; // Rigidbody = rb
+
+    public float forwardforce = 700f;
+    public float sidewaysForce = 500f;
+    public float upForce = 10f;
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("space"))
+            rb.AddForce(0, upForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+    }
+}
