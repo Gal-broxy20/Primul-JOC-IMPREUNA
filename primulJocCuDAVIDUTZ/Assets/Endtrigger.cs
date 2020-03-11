b306bca6985df42a0e9f5668442652044ee15920
yep_davidl <62029849+broxy20@users.noreply.github.com>
2020-03-11 17:29:21 +0200

    end trigger+ player movement stop

--- primulJocCuDAVIDUTZ/Assets/Endtrigger.cs
+++ primulJocCuDAVIDUTZ/Assets/Endtrigger.cs
@@ -2,8 +2,10 @@
 
 public class Endtrigger : MonoBehaviour
 {
+    public playerscript stop;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Level Finished");
+        stop.enabled = false;
     }
 }
