426a46d6e552649b88c8c27cd08b759846516066
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-15 12:03:37 +0200

    Completed Settings Menu

--- primulJocCuDAVIDUTZ/Assets/Scripts/SettingsManager.cs
+++ primulJocCuDAVIDUTZ/Assets/Scripts/SettingsManager.cs
@@ -1,11 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SettingsManager : MonoBehaviour
 {
     public AudioMixer audiomixer;
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+     void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = resolutions[ResolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetVolume(float volume)
     {
         audiomixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityindex)
+    {
+        QualitySettings.SetQualityLevel(qualityindex);
+    }
+
+    public void SetFullScreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
