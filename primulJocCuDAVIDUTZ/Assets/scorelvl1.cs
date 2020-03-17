254e2938022295ad7360ea96e25be7145a7e527a
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-17 19:17:44 +0200

    Fixed HighScore Mechanics in level 1

---
+++ primulJocCuDAVIDUTZ/Assets/scorelvl1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class scorelvl1 : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public int offset = 0;
+    public Text highScore;
+    public GameObject NewHighScoreText;
+    int suma;
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScoreLvl1", 0).ToString();
+    }
+    void Update()
+    {
+        suma = (int)player.position.z - offset;
+        scoreText.text = suma.ToString();
+        if (suma > PlayerPrefs.GetInt("HighScoreLvl1", 0))
+        {
+
+            PlayerPrefs.SetInt("HighScoreLvl1", suma);
+            highScore.text = suma.ToString();
+            NewHighScoreText.SetActive(true);
+        }
+
+    }
+}
