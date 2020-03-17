73f4adde88e275c6f42ad9089dc47270a11b2bec
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-17 19:13:13 +0200

    -Fixed highscore in lvl 2

---
+++ primulJocCuDAVIDUTZ/Assets/Scripts/scoreLvl2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreLvl2 : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public int offset = 0;
+    public Text highScore;
+    public GameObject NewHighScoreText;
+    int suma;
+     void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScoreLvl2", 0).ToString();
+    }
+    void Update()
+    {
+        suma = (int)player.position.z - offset;
+        scoreText.text = suma.ToString();
+        if(suma > PlayerPrefs.GetInt("HighScoreLvl2", 0))
+        {
+            
+            PlayerPrefs.SetInt("HighScoreLvl2", suma);
+            highScore.text = suma.ToString();
+            NewHighScoreText.SetActive(true);
+            
+        }
+        
+    }
+}
