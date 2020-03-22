60e62cfa48ce2b1b261801a46d68d965284ac93c
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-23 00:00:01 +0200

    Added Menu Fade In Animation + found sound bug in menu

---
+++ primulJocCuDAVIDUTZ/Assets/Scripts/FadeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeScene : MonoBehaviour
+{
+ 
+    public GameObject image;
+    public void SetImageInactive()
+    {
+        image.SetActive(false);
+    } 
+    
+}
---
+++ primulJocCuDAVIDUTZ/Assets/Scripts/UIClickSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIClickSound : MonoBehaviour
+{
+    public AudioSource uiclick;
+    public void PlaySoundOnUIClick()
+    {
+        uiclick.Play();
+
+    }
+}
