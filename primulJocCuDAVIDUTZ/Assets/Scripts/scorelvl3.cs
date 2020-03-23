9689cad1c9dbca7a8b3dc61b37e18409ae2ea4a6
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-23 11:09:15 +0200

    Changed some materials and Updated Level 3

---
+++ primulJocCuDAVIDUTZ/Assets/Scripts/scorelvl3.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class scorelvl3 : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public int offset = 0;
+    public Text highScore;
+    public GameObject NewHighScoreText;
+
+
+    public Text highScoreFall;
+    public GameObject NewHighScoreTextFall;
+    int suma;
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScoreLvl3", 0).ToString();
+        highScoreFall.text = highScore.text;
+    }
+    void Update()
+    {
+        suma = (int)player.position.z - offset;
+        scoreText.text = suma.ToString();
+        if (suma > PlayerPrefs.GetInt("HighScoreLvl3", 0))
+        {
+
+            PlayerPrefs.SetInt("HighScoreLvl3", suma);
+            highScore.text = suma.ToString();
+            highScoreFall.text = suma.ToString();
+            NewHighScoreText.SetActive(true);
+            NewHighScoreTextFall.SetActive(true);
+        }
+
+    }
+}
