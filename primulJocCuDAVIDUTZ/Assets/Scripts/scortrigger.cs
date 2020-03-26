e21be93dc7c306c60737fafbbdeb0007b14c1d0f
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-26 20:11:51 +0200

    UPDATEYEY

--- primulJocCuDAVIDUTZ/Assets/Scripts/scortrigger.cs
+++ primulJocCuDAVIDUTZ/Assets/Scripts/scortrigger.cs
@@ -1,14 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Audio;
 public class scortrigger : MonoBehaviour
 {
     public GameManager gameManager;
+    public GameObject skiptext;
+    public TutorialMovement movement;
     private void Start()
     {
         if (PlayerPrefs.GetInt("HasPlayedTutorial", 0) == 1)
-            gameManager.LoadNextLevel();
+            skiptext.SetActive(true);
+
+    }
+
+    public Animator animator;
+    private void Update()
+    {
+        if(PlayerPrefs.GetInt("HasPlayedTutorial", 0) == 1 && Input.GetKeyDown(KeyCode.Escape)) {
+            animator.SetBool("ESC", true);
+            StartCoroutine("LoadMenu");
+        }
+    }
+
+    public AudioSource source;
+    IEnumerator LoadMenu()
+    {
+        movement.enabled = false;
+        while(source.volume > 0f)
+        {
+            source.volume -= Time.deltaTime / 0.5f;
+            yield return null;
+        }
+        source.Stop();
+        yield return new WaitForSeconds(0.5f);
+        gameManager.LoadMenu();
     }
     private void OnTriggerEnter(Collider other)
     {
