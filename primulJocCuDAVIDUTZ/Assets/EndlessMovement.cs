3d448b34b74ddc89486078d6341f5bea241b1cb4
gal101 <62024153+gal101@users.noreply.github.com>
2020-03-27 20:47:44 +0200

    Created Endless mode, not finished

---
+++ primulJocCuDAVIDUTZ/Assets/EndlessMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessMovement : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    public float speed;
+    public Rigidbody rb;
+    public float sidewaysForce;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ primulJocCuDAVIDUTZ/Assets/spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime;
+
+    public Transform player;
+    public Vector3 offset;
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        transform.position = desiredPosition;
+        if (timeBtwSpawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+            if(startTimeBtwSpawn > minTime)
+            startTimeBtwSpawn -= decreaseTime;
+        } else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
